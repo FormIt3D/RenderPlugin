4a406aaa845c0cd285b74a5be93909aa324554a2
schaefm <matt.schaeffer@autodesk.com>
2021-03-03 12:09:17 -0700

    send image over ws

---
+++ components/main.js
@@ -0,0 +1,115 @@
+'use strict';
+
+import InstallerInstructions from "./installerInstructions.js";
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.socket = null;
+
+        this.state = {
+            socketIsConnected: false,
+            imageData: ''
+        };
+
+        this.createSocketConnection();
+    }
+
+    createSocketConnection(){
+        this.setState({socketIsConnected : false});
+
+        //TODO expose UI for user to enter port? or try port discovery?
+        const port = 9901;
+
+        this.socket = new WebSocket(`ws://localhost:${port}`);
+
+        this.socketConnectionCheckInterval = setInterval(() => {
+            if(this.state.socketIsConnected){
+                clearInterval(this.socketConnectionCheckInterval);
+            }
+        }, 1000);
+
+        this.socket.onopen = (event) => {
+            this.setState({socketIsConnected : true});
+            console.log('sending Plugin connected');
+            this.socket.send('Plugin connected');
+        };
+
+        this.socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+
+            switch(message.type){
+                case 'renderFinished':
+                    this.setState({'imageData': message.data});
+                    break;
+            }
+        }
+
+        this.socket.onerror = (event) => {
+            console.log('socket error', event);
+        }
+
+        this.socket.onclose = (event) => {
+            console.log('socket was closed', event)
+            this.setState({socketIsConnected : false});
+
+            //try to reconnect
+            this.createSocketConnection();
+        }
+    }
+
+    startRender(type){
+        this.socket.send(JSON.stringify({
+            type,
+            data: 'testdata'
+        }));
+    }
+
+    render() {
+
+        if(this.state.socketIsConnected){
+            return React.createElement(
+                'div',
+                {
+                    className: '',
+                },
+                [
+                    React.createElement(
+                        'a',
+                        {
+                            className: '',
+                            onClick: () => {this.startRender('renderArnold')}
+                        },
+                        'Start Arnold render!'
+                    ),
+                    React.createElement(
+                        'img',
+                        {
+                            className: '',
+                            src: `data:image/png;base64,${this.state.imageData}`
+                        },
+                    )
+                ]
+            );
+            
+        }else{
+            return React.createElement(
+                'div',
+                {
+                    className: ''
+                },
+                [
+                    'Plugin is unable to connect...',
+                    React.createElement(
+                        InstallerInstructions, 
+                        {},
+                        null
+                    )
+                ]
+            );
+        }
+    }
+}
+
+export default Main;
--- wsServer/index.js
+++ wsServer/index.js
@@ -1,11 +1,47 @@
 const WebSocket = require('ws');
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 const server = new WebSocket.Server({ port: 9901 });
 
+const renderArnold = async (obj) => {
+    const {socket, data} = obj;
+
+    //Hook into Arnold here.
+
+    //Temporary, send an image back.
+    const tempImageLocation = 'arnoldTemp.jpg'
+
+    const base64ImageData = await readFile(tempImageLocation, 'base64');
+
+    socket.send(JSON.stringify({
+        type:'renderFinished',
+        data: base64ImageData
+    }));
+}
+
 server.on('connection', (socket) => {
     socket.on('message', (message) => {
-        console.log('received: %s', message);
+        try{
+            message = JSON.parse(message);
+            console.log('received: %s', message);
+
+            switch(message.type){
+                case 'renderArnold': 
+                    renderArnold({
+                        socket,
+                        data: message.data
+                    });
+                    break;
+            }
+        }
+        catch(e){
+            //TODO handle initial ws message which can't be parsed.....
+            console.log(e);
+        }
     });
 
-    socket.send('something');
+    socket.send('Server received connection');
 });
